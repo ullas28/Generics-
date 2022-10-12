2805a8a0ff2c1e4efc27c5bece22cfe71652355b
unknown <you@example.com>
2022-10-12 11:01:34 +0530

    Added : Extended max() more than 3 parameters

--- com/bridgelabz/MaxUsingGenerics.java
+++ com/bridgelabz/MaxUsingGenerics.java
@@ -1,12 +1,13 @@
 /*Given 3 Integers find the maximum
  3 float find the maximum
- 3 strings find the greatestor max*/
+ 3 strings find the greatestor max
+ Extend the max method to take more then three parameters*/
 
 package com.bridgelabz;
 
 public class MaxUsingGenerics {
     //compareTo method for Checking Maximum values from 3 Integer Object
-    public static <T extends Comparable<T>> T max(T a, T b, T c) {
+    public static <T extends Comparable<T>> T max(T a, T b, T c, T d) {
         T max = a;
         if (b.compareTo(max) > 0) {
             max = b;
@@ -14,23 +15,26 @@ public class MaxUsingGenerics {
         if (c.compareTo(max)>0) {
             max = c;
         }
-        printMax(a,b,c,max);
+        if (d.compareTo(max)>0){
+            max = d;
+        }
+        printMax(a,b,c,d,max);
         return max;
     }
 
-    public static <T> void printMax(T a, T b, T c, T max){
-        System.out.printf("Max of %s %s and %s is %s\n",a,b,c,max);
+    public static <T> void printMax(T a, T b, T c, T d, T max){
+        System.out.printf("Max of %s %s %s and %s is %s\n",a,b,c,d,max);
     }
 
     public static void main(String[] args) {
-        Integer a = 190,b = 220, c= 300;
-        Float d = 12.20f,e = 19.20f, f= 31.2f;
-        String g = "Apple", h = "Peach", i = "Banana";
+        Integer a1 = 190,a2 = 220, a3 = 300, a4 = 230;
+        Float b1 = 12.20f,b2 = 19.20f, b3= 31.2f, b4 = 56.4F;
+        String c1 = "Apple", c2 = "Peach", c3 = "Banana", c4 = "PineApple";
         System.out.println("For Integer values");
-        max(a,b,c);
+        max(a1,a2,a3,a4);
         System.out.println("For Flaot values");
-        max(d,e,f);
+        max(b1,b2,b3,b4);
         System.out.println("For String values");
-        max(g,h,i);
+        max(c1,c2,c3,c4);
     }
 }
